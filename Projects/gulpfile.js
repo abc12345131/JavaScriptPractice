8d1908a87b4541c0dfcf245efc463cd3677e791b
abc12345131 <52730527+abc12345131@users.noreply.github.com>
2020-12-15 00:22:16 -0500

    install gulp-rename

--- Projects/gulpfile.js
+++ Projects/gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('fullstack',function(){
-
+    return gulp.src('src/js/*.js')
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('dist/js/'))
+    .pipe(uglify())
+    .pipe(rename({suffix:'.min'}))
+    .pipe(gulp.dest('dist/js/'))
 })
 
 gulp.task('default',[]);
