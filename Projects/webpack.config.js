7b6b6d8ed3da87342d018b9969ca8ec02dfabb0b
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-01-28 02:03:50 -0500

    Update webpack.config.js

--- Projects/webpack.config.js
+++ Projects/webpack.config.js
@@ -1,8 +1,15 @@
 //webpack use commonjs as 
 //use resolve to join the path
 const { resolve } = require('path');
+//install html-webpack-plugin to auto import js/css to html
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+//install mini-css-extract-plugin to extract css in separate file
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+//install optimize-css-assets-webpack-plugin to compress css file
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+
+//set env to development mode
+process.env.NODE_ENV = 'development';
 
 module.exports = {
     entry: './src/js/index.js',
@@ -13,8 +20,59 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.js$/,
+                //do not check imported node_modules 
+                exclude: /node_modules/,
+                /*
+                    set package.json 
+                    "eslintConfig": {
+                        "extends": "airbnb-base"
+                    }
+                */
+                //install eslint-config-airbnb-base eslint-plugin-import eslint
+                loader: 'eslint-loader',
+                options: {
+                    //auto fix the code
+                    fix: true        
+                }
+            },
+            {
+                test: /\.js$/,
+                //do not check imported node_modules 
+                exclude: /node_modules/,
+                //install babel-loader @babel/core @babel/preset-env
+                loader: 'babel-loader',
+                options: {
+                    //1.set compatibility options (only use @babel/preset-env only for basic compatibility)
+                    //2.for full compatibility install and import @babel/polyfill in js code, but the file will be huge
+                    //3.load on demand, install core-js
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                //load on demand
+                                useBuiltIns: 'usage',
+                                //core-js version
+                                corejs: {
+                                    version: 3
+                                },
+                                //target compatibility version
+                                targets: {
+                                    chrome: '60',
+                                    firefox: '60',
+                                    ie: '9',
+                                    safari: '10',
+                                    edge: '17'
+                                }
+                            }
+                        ]
+                    ]        
+                }
+            },
+            {
                 test: /\.less$/,
                 use: [
+                    //install style-loader css-loader less-loader
                     'style-loader',
                     'css-loader',
                     'less-loader'
@@ -24,14 +82,28 @@ module.exports = {
                 test: /\.css$/,
                 use: [
                     //use mini-css-extract-plugin loader replace style-loader
-                    //extract css in separate file
                     MiniCssExtractPlugin.loader,
                     'css-loader',
                     {
+                        //install postcss-loader postcss-preset-env
                         loader: 'postcss-loader',
                         options: {
                             ident: 'postcss',
                             plugins: () => [
+                                /*
+                                    set package.json 
+                                    "browserlist": {
+                                        "development": [
+                                            "last 1 chrome version",
+                                            "last 1 firefox version",
+                                            "last 1 safari version"],
+                                        "production": [
+                                            ">0.2%",
+                                            "not dead",
+                                            "not op_mini all"]
+                                    }
+                                */    
+                                //help plugin find setting of browserlist in package.json
                                 require('postcss-preset-env')()
                             ]
                         }
@@ -40,10 +112,11 @@ module.exports = {
             },
             {
                 test: /\.(jpg|png|gif)$/,
+                //install url-loader
                 loader: 'url-loader',
                 options: {
                     limit: 16 * 1024,
-                    // html-loader use commonjs, change default es6 to false
+                    //html-loader use commonjs, change default es6 to false
                     esModule: false, 
                     name: '[hash:10].[ext]',
                     outputPath: 'img'
@@ -51,10 +124,12 @@ module.exports = {
             },
             {
                 test: /\.html$/,
+                //install html-loader for html pics
                 loader: 'html-loader'
             },
             {
                 exclude: /\.(js|css|less|jpg|png|gif|html)$/,
+                //install file-loader for media
                 loader: 'file-loader',
                 options: {
                     name: '[hash:10].[ext]',
@@ -66,6 +141,7 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin(
             {
+                //auto import all packed js/css based on template
                 template:'./src/index.html'
             }
         ),
@@ -73,7 +149,8 @@ module.exports = {
             {
                 filename: 'css/build.css'
             }
-        )
+        ),
+        new OptimizeCssAssetsWebpackPlugin()
     ],
     mode: 'development',
 
