f32fad714acf4ce9604d479ba3d94dcd0b373c48
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-02-05 23:27:28 -0500

    Update index.js

--- Projects/src/js/index.js
+++ Projects/src/js/index.js
@@ -1,7 +1,24 @@
 import '../src/css/'
 
-if (module.hot) {
-    module.hot.accept('./modulechanged.js', function () {
-        functionchanged();
-    })
-}
+/*
+
+    if (module.hot) {
+        module.hot.accept('./modulechanged.js', function () {
+            functionchanged()
+        })
+    };
+*/
+
+
+
+// use import syntax to pack test as a separate file 
+// import(/* webpackChunkName: 'test' */'./test')
+//     .then(() => {
+//         //file load successfully
+//         functionloaded()
+//     })
+//     .catch(() => {
+//         //file load failed
+//         console.log('File load failed!')
+//     });
+   
