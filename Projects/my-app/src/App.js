97e668218ede6801949ab624f926bfeb7ac32b06
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-03-17 14:59:15 -0400

    component structure

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import Component1 from './components/Component1';
+import Component2 from './components/Component2';
 
 export default class App extends Component {
   render(){
@@ -20,6 +22,8 @@ export default class App extends Component {
             Learn React
           </a>
         </header>
+        <Component1/>
+        <Component2/>
       </div>
     )
   }
