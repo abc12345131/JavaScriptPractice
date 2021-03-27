4705a0cb7504c48a15e7bf583ba27bc8b5aab43e
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-03-26 23:11:42 -0400

    add NavLink and  Route

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -2,13 +2,26 @@ import './App.css';
 import { Component } from 'react';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
+import {NavLink, Route} from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 export default class App extends Component {
   render(){
     return (
       <div className="App">
-        <Component1/>
-        <Component2/>
+        <div className="list-group">
+          <Component1/>
+          <Component2/>
+          {/* route link */}
+          <NavLink activeClassName="active" className="list-group-item" to="/about">About</NavLink>
+          <NavLink activeClassName="active" className="list-group-item" to="/home">Home</NavLink>
+        </div>
+        <div className="panel">
+          {/* register route */}
+          <Route path="/about" component={About}/>
+          <Route path="/home" component={Home}/>
+        </div>
       </div>
     )
   }
