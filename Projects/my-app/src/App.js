0e5a02fdef2034fa1693d2672b4363238437b0c3
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-03-27 21:57:27 -0400

    encapsulate NavLink

--- Projects/my-app/src/App.js
+++ Projects/my-app/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Component } from 'react';
 import Component1 from './components/Component1';
 import Component2 from './components/Component2';
-import {NavLink, Route} from 'react-router-dom';
+import {NavLink, Route, Switch} from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
 
@@ -14,15 +14,25 @@ export default class App extends Component {
           <Component1/>
           <Component2/>
           {/* route link */}
-          <NavLink activeClassName="active" className="list-group-item" to="/about">About</NavLink>
-          <NavLink activeClassName="active" className="list-group-item" to="/home">Home</NavLink>
+          <MyNavLink to="/about">About</MyNavLink>
+          <MyNavLink to="/home">Home</MyNavLink>
         </div>
         <div className="panel">
           {/* register route */}
-          <Route path="/about" component={About}/>
-          <Route path="/home" component={Home}/>
+          <Switch>
+            <Route path="/about" component={About}/>
+            <Route path="/home" component={Home}/>
+          </Switch>
         </div>
       </div>
+    )
+  }
+};
+
+export default class MyNavLink extends Component {
+  render() {
+    return (
+        <NavLink activeClassName="active" className="list-group-item" {...this.props}/>
     )
   }
 }
