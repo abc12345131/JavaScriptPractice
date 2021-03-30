6be1076f78742f4c875cd65c597fb96a29a64e00
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-03-30 09:13:21 -0400

    install craco

--- Projects/my-react-app/src/App.js
+++ Projects/my-react-app/src/App.js
@@ -5,12 +5,14 @@ import Header from './components/Header';
 import {Route, Switch, Redirect} from 'react-router-dom';
 import About from './pages/About';
 import Home from './pages/Home';
+import { Button } from 'antd';
 
 export default class App extends Component {
   render(){
     return (
       <div className="App">
         <Header/>
+        <Button type="primary">Button</Button>
         <div className="list-group">
           <MyNavLink to="/about">About</MyNavLink>
           <MyNavLink to="/home">Home</MyNavLink>
