f842fe41835a2f1080ad645704175a4f6dabdbff
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-04-17 22:55:09 -0400

    add login style

--- Projects/react-admin_client/src/pages/login/index.jsx
+++ Projects/react-admin_client/src/pages/login/index.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
 
 import './index.less'
+import logo from './images/logo.jpg'
 
 export default class Login extends Component {
     render() {
         return (
-            <div>
-                Login
+            <div className="login">
+                <header className="login">
+                    <img src={logo} alt="logo"/>
+                    <h1>React Backstage Management System</h1>
+                </header>
+                <section className="content"></section>
             </div>
         )
     }
