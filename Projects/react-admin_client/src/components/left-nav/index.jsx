fac931c17a24f81fb34dff6e7752e3df124f2fb4
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-05-02 23:42:47 -0400

    optimize dynamic menu

--- Projects/react-admin_client/src/components/left-nav/index.jsx
+++ Projects/react-admin_client/src/components/left-nav/index.jsx
@@ -10,10 +10,19 @@ import {
   ContainerOutlined,
   MailOutlined,
 } from '@ant-design/icons';
+import menuList from '../../config/menuConfig'
 import logo from '../../assets/images/logo.jpg'
 import './index.less'
 
 export default class LeftNav extends Component {
+    
+    getMenuNodes = () => {
+        return menuList.map(item => {
+            if (!item.chirdren)
+            return 
+        })
+    }
+
     render() {
         const { SubMenu } = Menu;
 
@@ -29,15 +38,27 @@ export default class LeftNav extends Component {
                     mode="inline"
                     theme="dark"
                 >
-                    <Menu.Item key="1" icon={<PieChartOutlined />}>
-                        Home
+                    { this. getMenuNodes(menuList) }
+                    
+                    <Menu.Item key="1" icon={<AppstoreOutlined />}>
+                        <Link to="/home">Home</Link>
                     </Menu.Item>
                     <SubMenu key="sub1" icon={<MailOutlined />} title="Commodity">
-                        <Menu.Item key="5" icon={<MailOutlined />}>Category Management</Menu.Item>
-                        <Menu.Item key="6" icon={<MailOutlined />}>Product Management</Menu.Item>
+                        <Menu.Item key="2" icon={<MailOutlined />}>
+                            <Link to="/category">Category Management</Link>
+                        </Menu.Item>
+                        <Menu.Item key="3" icon={<MailOutlined />}>
+                            <Link to="/product">Product Management</Link>
+                        </Menu.Item>
                     </SubMenu>
+                    <Menu.Item key="4" icon={<PieChartOutlined />}>
+                        <Link to="/user">User Management</Link>
+                    </Menu.Item>
+                    <Menu.Item key="5" icon={<PieChartOutlined />}>
+                        <Link to="/role">Role Management</Link>
+                    </Menu.Item>
                     <SubMenu key="sub2" icon={<AppstoreOutlined />} title="Navigation Two">
-                        <Menu.Item key="9">Option 9</Menu.Item>
+                        <Menu.Item key="6">Option 9</Menu.Item>
                         <Menu.Item key="10">Option 10</Menu.Item>
                         <SubMenu key="sub3" title="Submenu">
                         <Menu.Item key="11">Option 11</Menu.Item>
--- Projects/react-admin_client/src/pages/admin/index.jsx
+++ Projects/react-admin_client/src/pages/admin/index.jsx
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { Layout } from 'antd'
 import memoryUtils from '../../utils/memoryUtils'
 import LeftNav from '../../components/left-nav'
 import Header from '../../components/header'
-
+import Home from '../../pages/home'
+import Category from '../../pages/category'
+import Product from '../../pages/product'
+import Role from '../../pages/role'
+import User from '../../pages/user'
+import Bar from '../../pages/charts/bar'
+import Line from '../../pages/charts/line'
+import Pie from '../../pages/charts/pie'
 
 const { Footer, Sider, Content } = Layout
 
@@ -21,7 +28,19 @@ export default class Admin extends Component {
                 </Sider>
                 <Layout>
                     <Header/>
-                    <Content>Content</Content>
+                    <Content>
+                        <Switch>
+                            <Route path="/home" component={Home}/>
+                            <Route path="/category" component={Category}/>
+                            <Route path="/product" component={Product}/>
+                            <Route path="/role" component={Role}/>
+                            <Route path="/user" component={User}/>
+                            <Route path="/charts/bar" component={Bar}/>
+                            <Route path="/charts/line" component={Line}/>
+                            <Route path="/charts/pie" component={Pie}/>
+                            <Redirect to="/home"/>
+                        </Switch>
+                    </Content>
                     <Footer style={{textAlign: 'center', color: '#cccccc'}}>Use Google Chrome for better performance</Footer>
                 </Layout>
             </Layout>
