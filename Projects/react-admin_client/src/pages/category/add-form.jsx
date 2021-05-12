452e73461bebe1530bdd9644caa54698a1d328f5
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-05-12 18:23:06 -0400

    add add-form

---
+++ Projects/react-admin_client/src/pages/category/add-form.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Form, Select, Input } from 'antd'
+import PropTypes from 'prop-types'
+
+const Item = Form.Item
+const Option = Select.Option
+
+export default class AddForm extends Component {
+
+    formRef = React.createRef()
+
+    static propTypes = {
+        categories: PropTypes.array.isRequired,
+        parentId: PropTypes.string.isRequired,
+        setFormRef: PropTypes.func.isRequired
+    }
+
+    constructor (props) {
+        super(props)
+        this.props.setFormRef(this.formRef)
+    }
+
+    render() {
+
+        const { categories, parentId } = this.props
+
+        return (
+            <Form initialValues={{classification: parentId}} ref={this.formRef}>
+                <Item name='classification'>
+                    <Select>
+                        <Option value='0'>Primary Classification</Option>
+                        {
+                            categories.map(c => (<Option value={c._id}>{c.name}</Option>))
+                        }
+                    </Select>
+                </Item>
+                <Item name='category'>
+                    <Input placeholder='Please input claasification name'></Input>
+                </Item>
+            </Form>
+        )
+    }
+}
