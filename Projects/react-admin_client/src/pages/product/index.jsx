bc9df931d8cc70aa89c609fa68d4f92a26fa0d7b
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-05-16 22:52:16 -0400

    add product sub-route

--- Projects/react-admin_client/src/pages/product/index.jsx
+++ Projects/react-admin_client/src/pages/product/index.jsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
+import ProductAddUpdate from './add-update'
+import ProductDetail from './detail'
+import ProductHome from './home'
 
 export default class Product extends Component {
     render() {
         return (
-            <div>
-                Product
-            </div>
+            <Switch>
+                <Route exact path='/product' component={ProductHome}/>
+                <Route path='/product/addupdate' component={ProductAddUpdate}/>
+                <Route path='/product/detail' component={ProductDetail}/>
+                <Redirect to='/product'/>
+            </Switch>
         )
     }
 }
