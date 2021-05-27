192a12b931ef99b48bb8e5fc1fc992a2d04553bf
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-05-27 01:15:14 -0400

    add add-form

---
+++ Projects/react-admin_client/src/pages/role/add-form.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Form, Input } from 'antd'
+import PropTypes from 'prop-types'
+
+const {Item} = Form
+
+export default class AddForm extends Component {
+
+    static propTypes = {
+        setFormRef: PropTypes.func.isRequired,
+    }
+
+    constructor (props) {
+        super(props)
+        this.formRef = React.createRef()
+        this.props.setFormRef(this.formRef)
+    }
+
+    render() {
+
+        const formItemLayout = {
+            labelCol: {
+              span: 6 
+            },
+            wrapperCol: {
+              span: 16 
+            }
+        }
+
+        return (
+            <Form ref={this.formRef} {...formItemLayout}>
+                <Item 
+                    name='roleName'
+                    label='Role Name'
+                    rules={[
+                        {required: true, message: 'role name is required'}
+                    ]}
+                >
+                    <Input placeholder='Please input role name'></Input>
+                </Item>
+            </Form>
+        )
+    }
+}
