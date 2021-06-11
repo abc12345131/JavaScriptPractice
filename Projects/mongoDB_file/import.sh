2467e7cb13208b6c1cf3eb31033c95e5525281d2
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-06-11 18:31:00 -0400

    bug fix

--- Projects/mongoDB_file/import.sh
+++ Projects/mongoDB_file/import.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-mongoimport --db react-app --collection users --drop --file mongoDB_file\users.json --jsonArray
-mongoimport --db react-app --collection products --drop --file mongoDB_file\products.json --jsonArray
-mongoimport --db react-app --collection roles --drop --file mongoDB_file\roles.json --jsonArray
-mongoimport --db react-app --collection categories --drop --file mongoDB_file\categories.json --jsonArray
-mongoimport --db react-app --collection work --drop --file mongoDB_file\work.json --jsonArray
+mongoimport --db react-app --collection users --drop --file users.json --jsonArray;
+mongoimport --db react-app --collection products --drop --file products.json --jsonArray;
+mongoimport --db react-app --collection roles --drop --file roles.json --jsonArray;
+mongoimport --db react-app --collection categories --drop --file categories.json --jsonArray;
+mongoimport --db react-app --collection work --drop --file work.json --jsonArray;
