31fa3b17a1c4d14cecc2dfa9afcecac41e2631c3
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-06-17 01:48:29 -0400

    add environment params

--- Projects/express-admin_server/app.js
+++ Projects/express-admin_server/app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
-var indexRouter = require('./routes/index');
-var fs = require('fs');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose')
+//redis for session cache
+// const redis = require('redis')
+// const session = require('express-session')
+const indexRouter = require('./routes/index');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT, REDIS_IP, REDIS_PORT, SESSION_SECRET } = require('./config/config')
 
-var app = express();
+
+const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,21 +32,59 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
-app.use('/api', indexRouter);
-
-//mongoose 
-mongoose.connect('mongodb://username:password@ip:27017/?authSource=admin')
+//mongoose
+//dev url
+//const mongoUrl = 'mongodb://localhost:27017/react-app'
+const mongoUrl = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/react-app?authSource=admin`
+const connectWithRetry = () => {
+  mongoose.connect(mongoUrl, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log('MongoDB is connected!')
   })
   .catch(error => {
     console.error('Failed to connect mongoDB!', error)
+    setTimeout(connectWithRetry, 5000)
   })
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
+}
 
+connectWithRetry()
+
+//redis
+// let RedisStore = require('connect-redis')(session)
+// let redisClient = redis.createClient({
+//   host: REDIS_IP,
+//   port: REDIS_PORT
+// })
+
+// redisClient.on('error', function (err) {
+//   console.log('Could not establish a connection with redis. ' + err);
+// })
+// redisClient.on('connect', function (err) {
+//   console.log('Connected to redis successfully');
+// })
+
+// app.use(
+//   session({
+//     store: new RedisStore({ client: redisClient }),
+//     secret: SESSION_SECRET,
+//     cookie: {
+//       secure: false,
+//       resave: false,
+//       saveUninitialized: false,
+//       httpOnly: true,
+//       maxAge: 3600000
+//     }
+//   })
+// )
+
+
+//api router
+app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
