86e3dfb605ebf3d0602af744d30975e57f2c432f
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-08-07 00:31:09 -0400

    add style to home page

--- Projects/vue-shop_client/src/components/FooterGuide/FooterGuide.vue
+++ Projects/vue-shop_client/src/components/FooterGuide/FooterGuide.vue
@@ -1,42 +1,46 @@
 <template>
   <div class="footer_guide">
-    <span class="guide_item" :class="{on: '/home'===$route.path}" @click="goTo('/home')">
+    <div class="guide_item" :class="{on: '/home'===$route.path}" @click="goTo('/home')">
       <span class="item_icon">
-        <i class="iconfont icon-waimai"></i>
+        <i class="iconfont icon-yangshi_icon_tongyong_home"></i>
       </span>
       <span>Home</span>
-    </span>
-    <a href="javascript:;" class="guide_item" :class="{on: '/search'===$route.path}" @click="goTo('/search')">
+    </div>
+    <div class="guide_item" :class="{on: '/search'===$route.path}" @click="goTo('/search')">
       <span class="item_icon">
-        <i class="iconfont icon-search"></i>
+        <i class="iconfont icon-yangshi_icon_tongyong_search"></i>
       </span>
       <span>Search</span>
-    </a>
-    <a href="javascript:;" class="guide_item" :class="{on: '/order'===$route.path}" @click="goTo('/order')">
+    </div>
+    <div class="guide_item" :class="{on: '/order'===$route.path}" @click="goTo('/order')">
       <span class="item_icon">
-        <i class="iconfont icon-dingdan"></i>
+        <i class="iconfont icon-yangshi_icon_tongyong_trips"></i>
       </span>
       <span>Order</span>
-    </a>
-    <a href="javascript:;" class="guide_item" :class="{on: '/profile'===$route.path}" @click="goTo('/profile')">
+    </div>
+    <div class="guide_item" :class="{on: '/profile'===$route.path}" @click="goTo('/profile')">
       <span class="item_icon">
-        <i class="iconfont icon-geren"></i>
+        <i class="iconfont icon-yangshi_icon_tongyong_my_account"></i>
       </span>
       <span>Profile</span>
-    </a>
+    </div>
   </div>
 </template>
 
 <script>
     export default {
-
+        methods: {
+            goTo(path) {
+                this.$router.replace(path)
+            }
+        }
     }
 </script>
 
 <style lang="sass" scoped>
     @import "../../assets/sass/mixins"
     .footer_guide  //footer
-        @include top-border-1px($white)
+        @include top-border-1px($grey)
         position: fixed
         z-index: 100
         left: 0
