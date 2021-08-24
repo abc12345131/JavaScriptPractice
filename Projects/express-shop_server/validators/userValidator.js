c575cd0f89c426898a24e85f65a90f90adb6ded1
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-08-24 13:21:28 -0400

    modify validator

--- Projects/express-shop_server/validators/userValidator.js
+++ Projects/express-shop_server/validators/userValidator.js
@@ -1,20 +1,11 @@
 const { body } = require('express-validator'); 
-const UserModel = require('../models/UserModel')
 const validate = require('../middlewares/validateMiddleware')
 
-exports.register = validate([
-    body('username').isLength({min:6, max:12}).withMessage('Username has to be 6-12 digit!'),
-    body('password').isLength({min:8, max:16}).withMessage('Password has to be 8-16 digit!'),
-    body('email')
-        .notEmpty().withMessage('Email cannot be empty!')
-        .isEmail().withMessage('Email is not valid!')
-        .bail()
-        .custom(email => {    
-            UserModel.findOne({email})
-                .then(user => {
-                    if (user) {
-                        res.send({status: 1, msg: 'Someone already uses this email, please try another one!'})
-                    }
-                })
-        })
+exports.usernameRegister = validate([
+    body('username').matches(/^\w{6,12}$/).withMessage('Username must include 6 to 12 digits the following character types: uppercase, lowercase, numbers, and _ symbol.'),
+    body('password').matches(/^(?![\d]+$)(?![a-zA-Z]+$)(?![^\da-zA-Z]+$).{8,16}$/).withMessage('Password should include 8 to 16 digits, at least two of the following character types: letters, numbers and special characters.')
+])
+
+exports.phoneRegister = validate([
+    body('phone').matches(/^[0-9]{10}$/).withMessage('Phone number must be 10 digits number only without space')
 ])
