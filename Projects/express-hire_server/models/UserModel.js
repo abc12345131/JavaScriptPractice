69d42262bdef17f907f6571616882b220d2be193
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2021-09-27 20:34:57 -0400

    bug fix

--- Projects/express-hire_server/models/UserModel.js
+++ Projects/express-hire_server/models/UserModel.js
@@ -5,6 +5,7 @@ const md5 = require('blueimp-md5')
 const userSchema = new mongoose.Schema({
     username: {type: String, required: true}, 
     password: {type: String, required: true, select: false}, 
+    userType: {type: String, required: true}, 
     phone: String,
     email: String,
     create_time: {type: Number, default: Date.now},
@@ -12,15 +13,5 @@ const userSchema = new mongoose.Schema({
 })
 
 const UserModel = mongoose.model('users', userSchema)
-
-// Initialize the default administrator user: admin/admin
-UserModel.findOne({username: 'admin'}).then(user => {
-    if(!user) {
-        UserModel.create({username: 'admin', password: md5('admin')})
-            .then(user => {
-                console.log('Initialize administrator user, username: admin, password: admin')
-            })
-    }
-})
 
 module.exports = UserModel
