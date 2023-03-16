6a4f5d3db677dbb738933c53a07c8620f50f0cab
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2023-03-16 15:30:47 -0400

    chore

--- Templates/TypeScript Redux template/redux/reducers/index.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { RootState } from '../types';
-import { todoReducer } from './todos';
-
-export const rootReducer = combineReducers<RootState>({
-  todos: todoReducer
-});
-
-export type { RootState };
