c319b52173ffd5aaf3d34d236b58b80e968a9c52
Baolong Wu <52730527+abc12345131@users.noreply.github.com>
2023-05-19 08:57:27 -0400

    chore

--- Practice/Customer_Information_Management_Software/src/com/baolongwu/project/Main.java
+++
@@ -1,9 +0,0 @@
-package com.baolongwu.project;
-
-public class Main {
-
-    public static void main(String[] args) {
-        CustomerView cView = new CustomerView();
-        cView.enterMainMenu();
-    }
-}
